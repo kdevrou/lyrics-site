366d34d7eeeb6759932309c09fc558ab3a942eb7
Kevin DeVrou <kevin.devrou@gmail.com>
2022-12-24 21:56:08 -0500

    add inner text

--- static/shortcodes/text-color.js
+++ static/shortcodes/text-color.js
@@ -6,17 +6,23 @@ CMS.registerEditorComponent({
             label: "Color",
             widget: "string"
         },
+        {
+            color: "text",
+            label: "Text",
+            widget: "markdown"
+        },
     ],
-    pattern: /^{{< gist ([a-zA-Z0-9]+) >}}/,
+    pattern: new RegExp(`^{{< text-color ([a-zA-Z0-9]+) >}}("[a-zA-Z\\s0-9:!@#\$%\/\^\&*\)\(+=._-]+"){{< /text-color >}}`),
     fromBlock: function(match) {
         return {
             color: match[1],
+            text: match[2],
         };
     },
     toBlock: function(obj) {
-        return `{{< text-color ${obj.color} >}}`;
+        return `{{< text-color ${obj.color} >}}${obj.text}{{< /text-color >}}`;
     },
     toPreview: function(obj) {
-        return `{{< text-color ${obj.color} >}}`;
+        return `{{< text-color ${obj.color} >}}${obj.text}{{< /text-color >}}`;
     },
 });
